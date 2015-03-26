17c86581c1e8fac9c729388c1f1f73319fff4069
Jakub Argasiński <jakub@argasinski.eu>
2015-03-26 01:05:42 +0100

    Add support for multiple languages. Add Polish translation for competitions.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,42 +2,68 @@ var angular = require('angular');
 
 var application = angular.module('Riverwash.app', ['ui.router']);
 
-application.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+function getDefaultLanguage() {
+    var androidLang;
+    var defaultLang;
+    var isPolish;
+
+    // works for earlier version of Android (2.3.x)
+    if (navigator && navigator.userAgent && (androidLang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) {
+        defaultLang = androidLang[1];
+    } else {
+        // works for iOS and Android 4.x
+        defaultLang = navigator.userLanguage || navigator.language;
+    }
+
+    isPolish = ['pl_PL', 'pl'].indexOf(defaultLang) !== -1;
+    defaultLang = (isPolish ? 'pl' : 'en');
+
+    return defaultLang;
+}
+
+application.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $urlMatcherFactoryProvider) {
+    var defaultLanguage;
+
     $locationProvider.html5Mode(true);
+    $urlMatcherFactoryProvider.strictMode(false);
 
-    $urlRouterProvider.otherwise('/');
+    defaultLanguage = getDefaultLanguage();
 
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: '/pages/en/home.html'
+    $urlRouterProvider.otherwise('/' + defaultLanguage);
+
+    function languageTemplateUrl(template) {
+        return function ($stateParams) {
+            var isSupportedLanguage = ['en', 'pl'].indexOf($stateParams.lang) !== -1;
+            var lang = (isSupportedLanguage ? $stateParams.lang : defaultLanguage);
+            return '/pages/' + lang + '/' + template + '.html';
+        }
+    }
+
+    $stateProvider.state('app', {
+        abstract: true,
+        url: '/{lang}',
+        template: '<ui-view></ui-view>'
     });
 
-    $stateProvider.state('location', {
-        url: '/location',
-        templateUrl: '/pages/en/location.html'
+    $stateProvider.state('app.root', {
+        url: '',
+        templateUrl: languageTemplateUrl('home')
     });
 
-    $stateProvider.state('schedule', {
-        url: '/schedule',
-        templateUrl: '/pages/en/schedule.html'
+    var states = [
+        'location',
+        'schedule',
+        'tickets',
+        'travelling',
+        'competitions'
+    ];
+
+    states.forEach(function(state) {
+        $stateProvider.state('app.' + state, { url: '/' + state, templateUrl: languageTemplateUrl(state) });
     });
 
-    // Travelling and accommodation
-    $stateProvider.state('tickets', {
-        url: '/tickets',
-        templateUrl: '/pages/en/tickets.html'
-    });
+});
 
-    // Travelling and accommodation
-    $stateProvider.state('travelling', {
-        url: '/travelling',
-        templateUrl: '/pages/en/travelling.html'
-    });
-
-    // Rules and competitions
-    $stateProvider.state('competitions', {
-        url: '/competitions',
-        templateUrl: '/pages/en/competitions.html'
-    });
-
+application.controller('applicationController', function ($scope, $stateParams) {
+    return $scope.lang = $stateParams.lang;
 });
