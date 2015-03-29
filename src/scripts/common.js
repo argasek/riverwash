980a46bdee299a416475cfb0f781a8df8da71106
Jakub Argasiński <jakub@argasinski.eu>
2015-03-29 22:29:28 +0200

    Load jQuery and lodash from CDN.

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,7 +1,6 @@
-require('jquery');
 require('angular');
 require('angular-translate');
 require('angular-ui-router');
 require('angular-google-maps');
-require('lodash');
-require('bootstrap');
+require('bootstrap/collapse');
+require('bootstrap/transition');
